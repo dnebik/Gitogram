337bdeb13ae06870bcd7004e733bfe521c9b6870
dnebik <ya.dneb@yandex.ru>
2022-01-15 06:27:22 +0300

    верстка

---
+++ src/components/TheHeader/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__top-line">
+        <span class="header-logo">Gitogram /</span>
+        <app-icon>home</app-icon>
+      </div>
+      <div class="header__content" v-if="$slots.default">
+        <slot />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import AppIcon from '@/components/App/AppIcon';
+
+export default {
+  name: 'TheHeader',
+  components: { AppIcon },
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  &__top-line {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <the-header />
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader';
+
+export default {
+  name: 'Home',
+  components: { TheHeader },
+};
+</script>

f7fd8222dce0be79b4eb35ffaaaefd7a76976800
dnebik <ya.dneb@yandex.ru>
2022-01-23 15:35:47 +0300

    remove

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <app-progressbar/>
     <the-header class="home__header">
       <follow-line/>
     </the-header>
@@ -12,12 +11,11 @@
 import TheHeader from '@/components/TheHeader';
 import FollowLine from '@/components/FollowLine';
 import WallPosts from '@/components/WallPosts';
-import AppProgressbar from '@/components/App/AppProgressbar';
 
 export default {
   name: 'Home',
   components: {
-    AppProgressbar, WallPosts, FollowLine, TheHeader,
+    WallPosts, FollowLine, TheHeader,
   },
 };
 </script>
